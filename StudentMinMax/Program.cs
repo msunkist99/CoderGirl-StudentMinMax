631b559f9d090dbb9bab6e0b5f471325c30697a1
msunkist99 <msunkist99@gmail.com>
2019-03-02 14:14:41 -0600

    2019/03/02 initial code

--- StudentMinMax/Program.cs
+++ StudentMinMax/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace StudentMinMax
 {
@@ -6,6 +7,35 @@ namespace StudentMinMax
     {
         public static void Main()
         {
+            // read from the studentdata.txt file
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] studentsText = File.ReadAllLines(@"C:\Users\msunk\Documents\CoderGirlLC101\Lesson09 CoderGirl-StudentMinMax\CoderGirl-StudentMinMax\studentdata.txt");
+
+            // create an array of Student classes from thestudentdata text
+            Student[] students = new Student[studentsText.Length];
+
+            for (int i = 0; i < studentsText.Length; i++)
+            {
+                {
+                    string[] studentTextInputFields = studentsText[i].Split(" ");
+
+                    Student student = new Student();
+                    student.Name = studentTextInputFields[0];
+
+                    int[] scores = new int[studentTextInputFields.Length - 1];
+
+                    for (int j = 1; j < studentTextInputFields.Length; j++)
+                    {
+                        scores[j - 1] = int.Parse(studentTextInputFields[j]);
+                        
+                    }
+                    student.Scores = scores;
+                    students[i] = student;
+                }
+            }
+
+
             foreach (var student in students)
             {
                 Console.WriteLine($"{student.Name} Min: {student.GetMinimumScore()} Max: {student.GetMaximumScore()}");
--- StudentMinMax/Student.cs
+++ StudentMinMax/Student.cs
@@ -8,10 +8,32 @@
 
         public int? GetMaximumScore()
         {
+            int maxScore = 0;
+
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                if (Scores[i] > maxScore)
+                {
+                    maxScore = Scores[i];
+                }
+            }
+
+            return maxScore;
         }
 
         public int? GetMinimumScore()
         {
+            int minScore = 100;
+
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                if (Scores[i] < minScore)
+                {
+                    minScore = Scores[i];
+                }
+            }
+
+            return minScore;
         }
     }
 }
